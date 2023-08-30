9fda9d4b9f1a862444334587663b18593035846c
Hakan Okay <hokay18@gmail.com>
2023-08-30 01:30:24 -0400

    added margin to experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,7 +13,7 @@ import { useSectionInView } from "@/lib/hooks";
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
   return (
-    <section id="experience" ref={ref}>
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>Experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
