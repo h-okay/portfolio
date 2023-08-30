f10fa871789a7d944c7b2713b42ae32d7c6ab42c
Hakan Okay <hokay18@gmail.com>
2023-08-30 00:00:13 -0400

    Added Experience section

---
+++ components/experience.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import SectionHeading from "@/components/section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { Fragment } from "react";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+  return (
+    <section id="experience" ref={ref}>
+      <SectionHeading>Experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "center",
+                padding: "1.3rem 2rem ",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              dateClassName={index % 2 === 0 ? "text-left" : "text-right"}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-800">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
