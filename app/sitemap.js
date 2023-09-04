45a0658b03b6a3f503f51ab94798c02ded832323
Hakan Okay <hokay18@gmail.com>
2023-09-04 08:41:43 -0400

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,11 @@
+const URL = "https://hakanokay.dev";
+
+const ROUTES = [""];
+
+export default async function sitemap() {
+  const routes = ROUTES.map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+  return [...routes];
+}
