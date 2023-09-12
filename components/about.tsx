a1cbdea5a7c935d7eb72257e487637a42da3c797
Hakan Okay <hokay18@gmail.com>
2023-09-12 17:40:45 -0400

    Update about section

--- components/about.tsx
+++ components/about.tsx
@@ -18,23 +18,29 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        My journey in the tech world began with a deep fascination for data
-        science and its potential to uncover valuable insights from complex
-        datasets. As a Data Scientist, I honed my skills in machine learning,
-        data analysis, and visualization, working on projects that involved
-        solving real-world challenges with data-driven solutions. I had the
-        chance to collaborate with some of the biggest gaming companies in
-        Europe, which provided me with invaluable experiences and a deep
-        understanding of the industry's inner workings.
+        My journey in the tech world began with a deep fascination for{" "}
+        <span className="font-bold">data science</span> and its potential to
+        uncover valuable insights from complex datasets. As a Data Scientist, I
+        honed my skills in <span className="font-bold">machine learning</span>,
+        <span className="font-bold">&nbsp;data analysis</span>, and
+        <span className="font-bold">&nbsp;data visualization</span>, working on
+        projects that involved solving real-world challenges with data-driven
+        solutions. I had the chance to collaborate with some of the{" "}
+        <span className="font-bold">biggest gaming companies</span> in Europe,
+        which provided me with invaluable experiences and a deep understanding
+        of the industry's inner workings.
       </p>
       <p>
         After I decided to learn web development to be able to provide
-        end-to-end, full-stack solutions to companies so that the insights can
-        be easily shared with relevant stakeholders. I added various web
-        development technologies to my skillset, including React, Next.js, and
-        Tailwind CSS. Currently, I'm working as a freelance software developer, helping
-        companies build their online presence and create data-driven web
-        applications.
+        <span className="font-bold">&nbsp;end-to-end</span>,
+        <span className="font-bold">&nbsp;full-stack</span> solutions to
+        companies so that the insights can be easily shared with relevant
+        stakeholders. I added various web development technologies to my
+        skillset, including React, Next.js, and Tailwind CSS. Currently, I'm
+        working as a
+        <span className="font-bold">&nbsp;Freelance Software Developer</span>,
+        helping companies build their online presence and create
+        <span className="font-bold">&nbsp;data-driven web applications</span>.
       </p>
     </motion.section>
   );
