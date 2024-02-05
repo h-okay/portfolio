1da7ffff7816c31260fdef4adf15ea8084a0f1bf
Hakan Okay <hokay.ca@gmail.com>
2024-02-04 22:57:14 -0500

    Update content

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import SectionHeading from '@/components/section-heading';
-import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
   const { ref } = useSectionInView('About');
@@ -17,30 +17,15 @@ export default function About() {
       id='about'
     >
       <SectionHeading>About me</SectionHeading>
-      <p className='mb-3'>
-        My journey in the tech world began with a deep fascination for{' '}
-        <span className='font-bold'>data science</span> and its potential to
-        uncover valuable insights from complex datasets. As a Data Scientist, I
-        honed my skills in <span className='font-bold'>machine learning</span>,
-        <span className='font-bold'>&nbsp;data analysis</span>, and
-        <span className='font-bold'>&nbsp;data visualization</span>, working on
-        projects that involved solving real-world challenges with data-driven
-        solutions. I had the chance to collaborate with some of the{' '}
-        <span className='font-bold'>biggest gaming companies</span> in Europe,
-        which provided me with invaluable experiences and a deep understanding
-        of the industry's inner workings.
+      <p className='mb-3 text-xl tracking-wide'>
+        I'm a versatile software developer with the experience of developing web applications and services,
+        creating efficient data pipelines and training/deploying machine learning models for different purposes.
       </p>
-      <p>
-        After I decided to learn web development to be able to provide
-        <span className='font-bold'>&nbsp;end-to-end</span>,
-        <span className='font-bold'>&nbsp;full-stack</span> solutions to
-        companies so that the insights can be easily shared with relevant
-        stakeholders. I added various web development technologies to my
-        skillset, including React, Next.js, and Tailwind CSS. Currently, I'm
-        working as a
-        <span className='font-bold'>&nbsp;Freelance Software Developer</span>,
-        helping companies build their online presence and create
-        <span className='font-bold'>&nbsp;data-driven web applications</span>.
+      <br/>
+      <p className='mb-3 text-xl tracking-wide'>
+        I'm fluent in Python, SQL, and English. I've also written a good amount of TypeScript, JavaScript, HTML, and
+        CSS. I have some exposure to Go and Java. I learn quickly, care about detail and I like finishing tasks and
+        focusing on the results.
       </p>
     </motion.section>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,13 +2,13 @@
 
 import { BsArrowRight, BsGithub, BsLinkedin } from 'react-icons/bs';
 
-import { useActiveSectionContext } from '@/context/active-section-context';
-import { useSectionInView } from '@/lib/hooks';
-import { motion } from 'framer-motion';
+import { HiDownload } from 'react-icons/hi';
 import Image from 'next/image';
 import Link from 'next/link';
-import { HiDownload } from 'react-icons/hi';
 import { TfiWrite } from 'react-icons/tfi';
+import { motion } from 'framer-motion';
+import { useActiveSectionContext } from '@/context/active-section-context';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Intro() {
   const { ref } = useSectionInView('Home');
@@ -57,7 +57,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         Hello, <span className='font-semibold'>I'm Hakan.</span> I'm a Software
-        Developer with 3 years of experience. I ❤️ building{' '}
+        Developer with 3+ years of experience. I ❤️ building{' '}
         <span className='font-semibold'>web applications</span> to solve
         real-world problems and share{' '}
         <span className='font-semibold'>data-driven insights.</span>
