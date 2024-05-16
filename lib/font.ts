89ac301a72e3c26b6a2fee194b32e663f3b53373
Hakan Okay <hokay.ca@gmail.com>
2024-05-16 14:44:37 -0400

    Add more fonts

--- lib/font.ts
+++ lib/font.ts
@@ -1,3 +1,17 @@
-import { Inter } from "next/font/google"
+import { Bebas_Neue, Inter, Poppins } from "next/font/google"
 
-export const inter = Inter({ subsets: ["latin"] })
+export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
+
+export const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bebas",
+  weight: "400",
+})
