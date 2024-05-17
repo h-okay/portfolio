0c2c5a25c8b16ab5cee639c7edb138632715dd9b
Hakan Okay <hokay.ca@gmail.com>
2024-05-17 12:55:06 -0400

    Hide button instead of turning opacity down when not mounted

--- components/scroll-to-top.tsx
+++ components/scroll-to-top.tsx
@@ -9,19 +9,14 @@ const ScrollToTopButton = () => {
 
   useEffect(() => {
     const toggleVisibility = () => {
-      // if the user scrolls down, show the button
       window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false)
     }
-    // listen for scroll events
     window.addEventListener("scroll", toggleVisibility)
-
-    // clear the listener on component unmount
     return () => {
       window.removeEventListener("scroll", toggleVisibility)
     }
   }, [])
 
-  // handles the animation when scrolling to the top
   const scrollToTop = () => {
     isVisible &&
       window.scrollTo({
@@ -34,7 +29,7 @@ const ScrollToTopButton = () => {
     <button
       className={cn(
         "fixed bottom-20 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950",
-        isVisible ? "opacity-100" : "opacity-0"
+        isVisible ? "flex" : "hidden"
       )}
       onClick={scrollToTop}
     >
